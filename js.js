51094dc0a2b57f1fe3b04a453867624e2a791037
AngelaMallet <angelamallet@gmail.com>
2018-10-11 10:33:04 +0200

    ex6

---
+++ js.js
@@ -0,0 +1,8 @@
+// j'ouvre le document ou j'appelle l'id de l'image1. quand je passe sur l'image avec
+// la souris je sollicite une action
+function change(Fabin){
+  Fabin.src='images/'+Fabin.id+'_2.jpg';
+};
+function out(Fabin){
+  Fabin.src='images/'+Fabin.id + '.jpg';
+}
